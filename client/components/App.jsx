9e491f648dd7cee93edbfd93b001e78a010a32e1
zoe-joblin <zoe.joblin@gmail.com>
2021-11-03 11:50:51 +1300

    initial commit

---
+++ client/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Form from './Form'
+
+function App () {
+
+  return (
+    <>
+    {/* <h1> Kia ora </h1> */}
+    < Form />
+    </>
+  )
+}
+
+export default App
